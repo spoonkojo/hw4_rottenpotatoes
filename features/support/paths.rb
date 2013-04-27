3c1c1b75929d7d5bc98c82b977f601ecaaf3233e
spoonkojo <spoonkojo@gmail.com>
2013-04-27 12:22:47 -0600

    Add feature support paths

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -22,6 +22,18 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+    when /^the movies page$/ then '/movies'
+
+    when /^the edit page for "(.*)"$/
+      edit_movie_path Movie.find_by_title($1)
+
+    when /^the details page for "(.*)"$/
+      movie_path Movie.find_by_title($1)
+
+    when /^the Similar Movies page for "(.*)"$/
+      same_director_path Movie.find_by_title($1)
+
     else
       begin
         page_name =~ /^the (.*) page$/
